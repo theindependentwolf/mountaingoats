3574f127ac1dd34118a26d7903030c8a00a77160
Aniruth Oblah <aniruth.oblah@gmail.com>
2017-09-17 21:15:14 -0400

    finishing touches

--- js/mountaingoats.js
+++ js/mountaingoats.js
@@ -7,6 +7,15 @@ function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
+
+var player;
+var platforms;
+var cursors;
+var stars;
+var score = 0;
+var scoreText;
+
+
 function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -42,8 +51,68 @@ function create() {
     //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+
+     //  Finally some stars to collect
+    stars = game.add.group();
+    //  We will enable physics for any star that is created in this group
+    stars.enableBody = true;
+    //  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 12; i++)
+    {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
+        //  Let gravity do its thing
+        star.body.gravity.y = 300;
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
+
+    //  The score
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    //  Our controls.
+    cursors = game.input.keyboard.createCursorKeys();
     
 }
 
 function update() {
+	//  Collide the player and the stars with the platforms
+    game.physics.arcade.collide(player, platforms);
+
+
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+        player.frame = 4;
+    }
+    
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.body.velocity.y = -350;
+    }
+}
+
+
+function collectStar (player, star) {
+    
+    // Removes the star from the screen
+    star.kill();
 }
