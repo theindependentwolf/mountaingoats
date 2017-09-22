9dad122736aff3a4007788b7b47b820c670c00e8
Aniruth Oblah <aniruth.oblah@gmail.com>
2017-09-22 11:11:23 -0400

    game - bug

--- js/mountaingoats.js
+++ js/mountaingoats.js
@@ -14,10 +14,6 @@ var score = 0;
 var scoreText;
 
 
-function preload() {
-    //preload image
-}
-
 function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
